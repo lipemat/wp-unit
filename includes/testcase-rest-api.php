af15fdd47ffc4d8e800a3d47238d096cbcbaef08
lipemat <mat@matlipe.com>
2021-07-17 18:30:13 -0400

    Add `get_response` method to WP_Test_REST_TestCase
    Simple method to return the response of a mocked REST request.

--- includes/testcase-rest-api.php
+++ includes/testcase-rest-api.php
@@ -2,7 +2,6 @@
 
 abstract class WP_Test_REST_TestCase extends WP_UnitTestCase {
 	protected function assertErrorResponse( $code, $response, $status = null ) {
-
 		if ( is_a( $response, 'WP_REST_Response' ) ) {
 			$response = $response->as_error();
 		}
@@ -20,5 +19,21 @@ abstract class WP_Test_REST_TestCase extends WP_UnitTestCase {
 
 	protected function assertNotErrorResponse( $response ) {
 		$this->assertNotInstanceOf( 'WP_Error', $response );
+	}
+
+
+	/**
+	 * Mock a REST request and retrieve its response.
+	 *
+	 * @param string                $route  - e.g. /wp/v2/users
+	 * @param array                 $args   - Query parameters to pass.
+	 * @param GET|POST|PATCH|DELETE $method - Request method
+	 *
+	 * @return WP_REST_Response
+	 */
+	protected function get_response( string $route, array $args, string $method = 'POST' ) : WP_REST_Response {
+		$request = new \WP_REST_Request( $method, $route );
+		$request->set_query_params( $args );
+		return rest_get_server()->dispatch( $request );
 	}
 }
