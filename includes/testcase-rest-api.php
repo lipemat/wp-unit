d73d360e3847c329d584f8e4b646a5d3e7586567
lipemat <mat@matlipe.com>
2021-07-17 18:48:19 -0400

    Expand the assertNotErrorResponse method to include not WP_Error errors
    Translate an error response which is not a `WP_Error` into a possible
    error and check for `null`.

--- includes/testcase-rest-api.php
+++ includes/testcase-rest-api.php
@@ -1,6 +1,20 @@
 <?php
 
+/**
+ * Testcase for REST endpoints.
+ *
+ */
 abstract class WP_Test_REST_TestCase extends WP_UnitTestCase {
+	/**
+	 * Checks if the response is a WP_Error or a WP_REST_Response
+	 * which includes an error within.
+	 *
+	 * Verifies provided code matche error code.
+	 *
+	 * @param      $code
+	 * @param      $response
+	 * @param null $status
+	 */
 	protected function assertErrorResponse( $code, $response, $status = null ) {
 		if ( is_a( $response, 'WP_REST_Response' ) ) {
 			$response = $response->as_error();
@@ -17,8 +31,17 @@ abstract class WP_Test_REST_TestCase extends WP_UnitTestCase {
 	}
 
 
+	/**
+	 * Checks if the response is not a WP_Error nor a WP_REST_Response
+	 * which includes an error within.
+	 *
+	 * @param $response
+	 */
 	protected function assertNotErrorResponse( $response ) {
 		$this->assertNotInstanceOf( 'WP_Error', $response );
+		if ( is_a( $response, 'WP_REST_Response' ) ) {
+			$this->assertNull( $response->as_error() );
+		}
 	}
 
 
