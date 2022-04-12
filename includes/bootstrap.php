6ec33526110b91de59d14385c8f2980f1adb7309
lipemat <mat@matlipe.com>
2022-04-12 09:37:55 -0400

    Rename testcase.php to testcase-base.php to fix PHPStorm override
    Unable to resolve the file type override from a previous version.
    
    Does not change functionality, just file name.

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -367,7 +367,7 @@ if ( version_compare( tests_get_phpunit_version(), '6.0', '>=' ) ) {
 
 require __DIR__ . '/phpunit-adapter-testcase.php';
 require __DIR__ . '/abstract-testcase.php';
-require __DIR__ . '/testcase.php';
+require __DIR__ . '/testcase-base.php';
 require __DIR__ . '/testcase-http-remote-post.php';
 require __DIR__ . '/testcase-rest-api.php';
 require __DIR__ . '/testcase-rest-controller.php';
