f4c6f860d7fef7f1a85675db100cfe9fe2cd1f2f
lipemat <mat@matlipe.com>
2022-10-25 14:15:43 -0400

    Allow packages to install phpunit
    Instead of using a "replace" which prevents the phpunit package from
    being installed no mater where in the stack this package lives, we allow
    Yoast to require "phpunit/phpunit" and cleanup the extra files when the
    package is installed as a standalone.
    
    Opens greater possibilities for using this package in projects, which
    include their own phpunit versions while still allowing a clean
    standalone version with external phpunit by way of .phar.

---
+++ bin/cleanup-unused-dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+##################################################################
+#
+# `yoast/phpunit-polyfills` adds a bunch of phpunit
+# related requirements which are not used when this package
+# is used as a standalone.
+#
+# Instead of letting the superfluous dependencies clutter up
+# our IDE map, we remove them via the `post-*-cmd` composer scripts.
+#
+###################################################################
+
+# shellcheck disable=SC2039
+array=("vendor/yoast/" "vendor/composer/" )
+
+echo "Cleaning unused directories...".
+
+for directory in vendor/*/ ; do
+    # shellcheck disable=SC2039
+    if [[ ! "${array[*]}" =~ $directory ]]; then
+        rm -rf "$directory"
+    fi
+done;
+
+# Regenerate the autoloader with files removed.
+composer dump-autoload
