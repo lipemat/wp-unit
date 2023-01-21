ec4d79733903bcf6d4c8896dbd0f321a0af63ae7
lipemat <mat@matlipe.com>
2023-01-21 06:46:47 -0500

    Remove deprecated wp_get_ready_cron_jobs override
    Has not been needed since WP 5.1 and this library requires WP 5.1+

--- includes/template-tags/cron.php
+++ includes/template-tags/cron.php
@@ -1,7 +1,4 @@
 <?php
-
-global $wp_version;
-
 /**
  * Run all crons in cue during Testing
  *
@@ -24,63 +21,5 @@ function wp_cron_run_all() {
 				do_action_ref_array( $hook, $v['args'] );
 			}
 		}
-	}
-}
-
-/**
- * Placeholder for the core function which is available after WP Core 5.1
- *
- * @todo someday can remove this in favor of just using the core.
- *
- */
-if ( version_compare( $wp_version, '5.1', '<' ) ) {
-	/**
-	 * Retrieve cron jobs ready to be run.
-	 *
-	 * Returns the results of _get_cron_array() limited to events ready to be run,
-	 * ie, with a timestamp in the past.
-	 *
-	 * @since 5.1.0
-	 *
-	 * @return array Cron jobs ready to be run.
-	 */
-	function wp_get_ready_cron_jobs() {
-		/**
-		 * Filter to preflight or hijack retrieving ready cron jobs.
-		 *
-		 * Returning an array will short-circuit the normal retrieval of ready
-		 * cron jobs, causing the function to return the filtered value instead.
-		 *
-		 * @since 5.1.0
-		 *
-		 * @param null|array $pre Array of ready cron tasks to return instead. Default null
-		 *                        to continue using results from _get_cron_array().
-		 */
-		$pre = apply_filters( 'pre_get_ready_cron_jobs', null );
-		if ( null !== $pre ) {
-			return $pre;
-		}
-
-		$crons = _get_cron_array();
-
-		if ( false === $crons ) {
-			return [];
-		}
-
-		$gmt_time = microtime( true );
-		$keys     = array_keys( $crons );
-		if ( isset( $keys[0] ) && $keys[0] > $gmt_time ) {
-			return [];
-		}
-
-		$results = [];
-		foreach ( $crons as $timestamp => $cronhooks ) {
-			if ( $timestamp > $gmt_time ) {
-				break;
-			}
-			$results[ $timestamp ] = $cronhooks;
-		}
-
-		return $results;
 	}
 }
