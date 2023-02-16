51618e550cb4e8e3d2e77390acdde69bfa4194d4
lipemat <mat@matlipe.com>
2023-02-16 11:37:35 -0500

    Introduce `wp_cron_run_event` for running a cron event on demand
    Utility for running a specific cron event regardless of its ready state

--- includes/template-tags/cron.php
+++ includes/template-tags/cron.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Run all crons in cue during Testing
+ * Run all crons in queue during Testing
  *
  * @see wp/wp-cron.php
  *
@@ -18,6 +18,26 @@ function wp_cron_run_all() {
 				}
 
 				wp_unschedule_event( $timestamp, $hook, $v['args'] );
+				do_action_ref_array( $hook, $v['args'] );
+			}
+		}
+	}
+}
+
+/**
+ * Run a single cron event regardless of ready state.
+ *
+ * @param string $hook - Hook, which was registered with the event.
+ *
+ * @return void
+ */
+function wp_cron_run_event( $hook ) {
+	foreach ( _get_cron_array() as $crooks ) {
+		foreach ( (array) $crooks as $_hook => $keys ) {
+			if ( $hook !== $_hook ){
+				continue;
+			}
+			foreach ( $keys as $v ) {
 				do_action_ref_array( $hook, $v['args'] );
 			}
 		}
