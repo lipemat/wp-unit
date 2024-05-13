5b1e2551495a58dbf0cf959fbf031bd636320966
lipemat <mat@matlipe.com>
2024-05-13 16:50:08 -0400

    Fixup the wp-unit settings
    Use correct domain and no blog id.

--- dev/wp-unit/default-local-config.php
+++ dev/wp-unit/default-local-config.php
@@ -9,17 +9,16 @@
 $GLOBALS['memcached_servers'] = [ '127.0.0.1:11211' ];
 
 define( 'ABSPATH', 'E:/SVN/starting-point/wp/' );
-define( 'BLOG_ID_CURRENT_SITE', 2 );
 define( 'BOOTSTRAP', 'E:/SVN/wp-unit/includes/bootstrap.php' );
 define( 'DB_HOST', 'localhost' );
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_PASSWORD', 'hle1.qRA8W[BnR03' );
 define( 'DB_USER', 'wplibs' );
-define( 'DOMAIN_CURRENT_SITE', 'wp-libs.loc' );
-define( 'WP_TESTS_DOMAIN', 'wp-libs.loc' );
+define( 'DOMAIN_CURRENT_SITE', 'wp-unit.loc' );
+define( 'WP_TESTS_DOMAIN', 'wp-unit.loc' );
 define( 'WP_TESTS_EMAIL', 'unit-tests@test.com' );
 define( 'WP_TESTS_MULTISITE', true );
 define( 'WP_TESTS_SNAPSHOTS_BASE', 'Lipe\WP_Unit' );
-define( 'WP_TESTS_TITLE', 'WP Libs unit tests' );
+define( 'WP_TESTS_TITLE', 'WP Unit - Self Tests' );
 define( 'WP_UNIT_DIR', 'E:/SVN/wp-unit' );
 define( 'WP_SITE_ROOT', 'E:/SVN/starting-point/' );
