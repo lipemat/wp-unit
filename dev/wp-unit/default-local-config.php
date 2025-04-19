50a55f850e8fd6786a8878d87beb32d2ded7d9d1
lipemat <mat@matlipe.com>
2025-04-19 16:35:08 -0400

    Switch local config testing site to "plugins" instead of "starting-point"
    Set `WP_CONTENT_DIR` to resolve WP 6.8.
    
    Notice: Function wp_is_block_theme was called <strong>incorrectly</strong>
    
    Use `plugins` because it keeps inline with the same PHP version this project
    uses.

--- dev/wp-unit/default-local-config.php
+++ dev/wp-unit/default-local-config.php
@@ -8,17 +8,18 @@
 // Point to local memcache servers (Requirement of hosts like WPE).
 $GLOBALS['memcached_servers'] = [ '127.0.0.1:11211' ];
 
-define( 'ABSPATH', 'E:/SVN/starting-point/wp/' );
+define( 'ABSPATH', 'E:/SVN/plugins/wp/' );
 define( 'BOOTSTRAP', 'E:/SVN/wp-unit/includes/bootstrap.php' );
 define( 'DB_HOST', 'localhost' );
 define( 'DB_NAME', 'wordpress' );
 define( 'DB_PASSWORD', 'hle1.qRA8W[BnR03' );
 define( 'DB_USER', 'wplibs' );
 define( 'DOMAIN_CURRENT_SITE', 'wp-unit.loc' );
+define( 'WP_CONTENT_DIR', 'E:/SVN/plugins/content' );
 define( 'WP_TESTS_DOMAIN', 'wp-unit.loc' );
 define( 'WP_TESTS_EMAIL', 'unit-tests@test.com' );
 define( 'WP_TESTS_MULTISITE', true );
 define( 'WP_TESTS_SNAPSHOTS_BASE', 'Lipe\WP_Unit' );
 define( 'WP_TESTS_TITLE', 'WP Unit - Self Tests' );
 define( 'WP_UNIT_DIR', 'E:/SVN/wp-unit' );
-define( 'WP_SITE_ROOT', 'E:/SVN/starting-point/' );
+define( 'WP_SITE_ROOT', 'E:/SVN/plugins/' );
