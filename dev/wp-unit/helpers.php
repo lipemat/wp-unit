3d2dc49758810020ee20d5c52e67fedc39d17274
lipemat <mat@matlipe.com>
2025-10-03 10:38:46 -0400

    Cleanup wp-unit helpers functions
    - Improve phpdocs
    - Remove unused bypass final call

--- dev/wp-unit/helpers.php
+++ dev/wp-unit/helpers.php
@@ -2,23 +2,22 @@
 /** @noinspection PhpExpressionResultUnusedInspection, PhpUnhandledExceptionInspection */
 declare( strict_types=1 );
 
-use DG\BypassFinals;
-
 /**
  * Version 2.6.3
  */
 
 /**
- * Call protected/private method of a class.
+ * Call a protected / private method of a class.
  *
- * @param class-string|object $object      An instantiated object or class name that we will run method on.
+ * @param class-string|object $object An instantiated object or class name that we will run a method on.
  * @param string              $method_name Method name to call.
  * @param array               $parameters  Array of parameters to pass into method.
  *
+ * @throws ReflectionException
  * @return mixed Method return.
  */
 function call_private_method( $object, string $method_name, array $parameters = [] ) {
-	$reflection = new \ReflectionClass( is_string( $object ) ? $object : get_class( $object ) );
+	$reflection = new \ReflectionClass( \is_string( $object ) ? $object : \get_class( $object ) );
 	$method = $reflection->getMethod( $method_name );
 	$method->setAccessible( true );
 
@@ -28,21 +27,22 @@ function call_private_method( $object, string $method_name, array $parameters = 
 /**
  * Get the value of a private constant or property from an object.
  *
- * @param class-string|object $object   An instantiated object or class name that we will run method on.
+ * @param class-string|object $object An instantiated object or class name that we will run a method on.
  * @param string              $property Property name or constant name to get.
  *
+ * @throws ReflectionException|LogicException
  * @return mixed
  */
 function get_private_property( $object, string $property ) {
-	$reflection = new \ReflectionClass( is_string( $object ) ? $object : get_class( $object ) );
+	$reflection = new \ReflectionClass( \is_string( $object ) ? $object : \get_class( $object ) );
 	if ( $reflection->hasProperty( $property ) ) {
 		$reflection_property = $reflection->getProperty( $property );
 		$reflection_property->setAccessible( true );
 		if ( $reflection_property->isStatic() ) {
 			return $reflection_property->getValue();
 		}
-		if ( is_string( $object ) ) {
-			throw new LogicException( 'Getging a non-static value from a non instantiated object is useless.' );
+		if ( \is_string( $object ) ) {
+			throw new LogicException( 'Getting a non-static value from a non-instantiated object is useless.' );
 		}
 		return $reflection_property->getValue( $object );
 	}
@@ -56,45 +56,20 @@ function get_private_property( $object, string $property ) {
  * @param string              $property Property name to set.
  * @param mixed               $value    Value to set.
  *
+ * @throws ReflectionException|LogicException
+ *
  * @return void
  */
 function set_private_property( $object, string $property, $value ): void {
-	$reflection = new \ReflectionClass( is_string( $object ) ? $object : get_class( $object ) );
+	$reflection = new \ReflectionClass( \is_string( $object ) ? $object : \get_class( $object ) );
 	$reflection_property = $reflection->getProperty( $property );
 	$reflection_property->setAccessible( true );
 	if ( $reflection_property->isStatic() ) {
 		$reflection_property->setValue( null, $value );
 	} else {
-		if ( is_string( $object ) ) {
-			throw new LogicException( 'Setting a non-static value on a non instantiated object is useless.' );
+		if ( \is_string( $object ) ) {
+			throw new LogicException( 'Setting a non-static value on a non-instantiated object is useless.' );
 		}
 		$reflection_property->setValue( $object, $value );
 	}
-}
-
-/**
- * Allow extending a particular final class.
- *
- * Typically used with `change_container_object` when extending an
- * existing class with an anonymous class.
- *
- * We specify an allowed class to prevent tests from missing fatal errors with
- * inheriting from final classes.
- *
- * @notice   Must be called before the class is loaded into the application.
- *
- * @requires wp-unit 3.6.0+.
- *
- * @since    2.3.0
- *
- * @param class-string $class
- *
- * @return void
- */
-function allow_extending_final( string $class ): void {
-	static $bypass = [];
-	// Remove the global namespace to the directory structure is matched.
-	$bypass[] = '*/' . \implode( '/', \array_slice( \explode( '\\', $class ), 2 ) ) . '.php';
-	BypassFinals::enable();
-	BypassFinals::setWhitelist( \array_unique( $bypass ) );
 }
